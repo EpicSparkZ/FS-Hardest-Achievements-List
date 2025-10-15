aae9f124303d2a61b7b64a47d5af14aa5bc0c7cc
EpicSparkZ <epicsparkz@hotmail.com>
2025-10-15 16:50:26 -0600

    Change redirect URL to 'N/A' in Changelog.js

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -2,7 +2,7 @@ export default {
   name: 'Other',
   mounted() {
     // Reload the page to ensure it loads fresh
-    window.location.replace('https://epicsparkz.pages.dev');
+    window.location.replace('N/A');
   },
   render() {
     // Return null or a placeholder while redirecting
