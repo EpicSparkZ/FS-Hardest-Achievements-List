24b8bcf999a4e7a46b5397aa0c80bb5df3029c48
EpicSparkZ <epicsparkz@hotmail.com>
2025-10-16 13:57:28 -0600

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -19,25 +19,11 @@ export default {
                 <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
-                </p>
-            </template>
-            <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Creator</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
                 <div class="type-title-sm">Completor</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <template v-else>
-                <div class="type-title-sm">Creators</div>
-                <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
-                    </template>
                 </p>
                 <div class="type-title-sm">Completor</div>
                 <p class="type-body">
